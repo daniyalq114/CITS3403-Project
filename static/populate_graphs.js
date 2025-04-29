36cb23c7a3eaeeb92c017cade83fce13936738b3
turn-my-swag-on <prosaman@outlook.com>
2025-04-29 22:48:37 +0800

    Added jinja to the visualise.html to attempt to construct the part one visualisation, based off some sample data I wrote. Rewrote the pokeapi calls in python instead.

--- static/populate_graphs.js
+++ static/populate_graphs.js
@@ -26,7 +26,6 @@ function constructGraph(moveinfo, graph) {
 
 // im so sorry for this code
 
-const xhr = new XMLHttpRequest();
 if(document.URL.includes("visualise")) {
     google.charts.setOnLoadCallback(drawFunction);
 }
@@ -34,22 +33,13 @@ if(document.URL.includes("visualise")) {
 
 var count = 0;
 function drawFunction() {
-    var poke_state;
-    var part1 = document.querySelectorAll(".content > .replay-record > .part1");
     var graphs = document.querySelectorAll(".content > .part3-container > .part3");
 
-    xhr.addEventListener('load', () => {
-        poke_state = JSON.parse(xhr.response);
-        part1[count].innerHTML = "<img src="+ poke_state.sprites.front_default + ">";
-    });
     const pokemon_count = pokemon.length;
     for(; count < pokemon_count; count++) {
         const moves = pokemon[count]["moves"];
         constructGraph([["Move", "Movecount"]].concat((moves.map((k, j) =>
         [moves[j][0], moves[j][1]]))), graphs[count]
-        );
-        xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + pokemon[count]["name"] +'/', true);
-        xhr.send(null);
-        
+        );  
     }
 }
