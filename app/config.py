b3f2974a66cbebb788b756fe0498a5f631b158d5
aradavi63 <adam.ahmadavi@gmail.com>
2025-05-12 11:39:41 +0800

    add testing configuration and deployment configuration

--- app/config.py
+++ app/config.py
@@ -11,3 +11,10 @@ instance_db_path = os.path.join(basedir, "instance", "app.db")
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or f"sqlite:///{instance_db_path}"
     SECRET_KEY = os.getenv('SECRET_KEY')
+
+class DeploymentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "instance", "app.db")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    TESTING = True
