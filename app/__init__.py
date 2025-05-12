b62d735f3105bbbfd5030a1fbc9cc0d809fbf187
aradavi63 <adam.ahmadavi@gmail.com>
2025-05-12 12:20:45 +0800

    relocate db initialisation

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,25 @@
 from flask import Flask
-from config import Config, db, migrate
+from config import Config
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config.from_object(Config)
+db = SQLAlchemy()
+migrate = Migrate()
 
-# --- Database setup ---
-db.init_app(app)
-migrate.init_app(app, db)
-
-# --- Flask login setup ---
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "login"
-
-csrf = CSRFProtect(app)
+def create_app():
+    """Create and configure the Flask application."""
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
+    csrf = CSRFProtect(app)
+    return app
 
 # Import routes after app initialization to avoid circular imports
 from routes import *
--- config.py
+++ config.py
@@ -1,9 +1,4 @@
 import os
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-migrate = Migrate()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 instance_db_path = os.path.join(basedir, "instance", "app.db")
